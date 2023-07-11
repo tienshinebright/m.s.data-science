2c0d93273dbbddbd01ee98ecf31d2ea597392e1d
zhangtienyong <tienhanprab@gmail.com>
2023-07-12 02:05:30 +0800

    dic

---
+++ python-crash-course/chapter_04/buffet.py
@@ -0,0 +1,10 @@
+foods = ('Prawn Tom Yum', 'Ranchuan Curry', 'Crispy Thai Prawn Cake', 'Prawn Pad Thai', 'Salted Beef in Coconut Milk')
+print(foods)
+for food in foods:
+    print("The thai buffet-restaurant offers: " + food)
+
+print("\nOur menu has been updated.")
+foods = ('Tom Kha Gai', 'Ranchuan Curry', 'Crispy Thai Prawn Cake', 'Phanaeng', 'Salted Beef in Coconut Milk')
+print(foods)
+for food in foods:
+    print("The thai buffet-restaurant offers: " + food)
