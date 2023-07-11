6c87df4d0a63ea919a08fad6a8b0ccb678179d41
zhangtienyong <tienhanprab@gmail.com>
2023-07-11 22:09:10 +0800

    sort

---
+++ python-crash-course/chapter_03/seeing_world.py
@@ -0,0 +1,13 @@
+locations = ['bangkok', 'hong kong', 'stuttgart', 'lucerne', 'philadelphia']
+print(locations)
+print(sorted(locations))
+print(locations)
+locations.sort(reverse=True)
+print(locations)
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+locations.sort()
+print(locations)
+locations.sort(reverse=True)
