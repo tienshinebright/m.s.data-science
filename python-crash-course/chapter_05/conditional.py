57800968a68beba1bd9d9901d218159907ffb162
zhangtienyong <tienhanprab@gmail.com>
2023-07-12 22:14:54 +0800

    conditional

---
+++ python-crash-course/chapter_05/conditional.py
@@ -0,0 +1,40 @@
+car = 'subaru'
+print("Is care == 'subaru' ? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi' ? I predict False.")
+print(car == 'audi')
+
+#Create at least 10 tests.
+book = 'python programming'
+language = 'javaScript'
+subject = 'computer science'
+topic = 'conditional test'
+role = 'engineer'
+position = 'senior'
+code_review = 'passed'
+unit_testing = 'not passed'
+data = 'empty'
+number = 8
+#5 tests evaluate to True
+print(book == 'python programming')
+print(language == 'javaScript')
+print(subject == 'computer science')
+print(topic == 'conditional test')
+print(role == 'engineer')
+print(position == 'senior')
+print(code_review == 'passed')
+print(unit_testing == 'not passed')
+print(data == 'empty')
+print(number == 8)
+#5 tests evaluate to False
+print(book == 'python')
+print(language == 'java')
+print(subject == 'computer')
+print(topic == 'if test')
+print(role == 'data engineer')
+print(position == 'junior')
+print(code_review == 'not passed')
+print(unit_testing == 'passed')
+print(data == 'full')
+print(number == 18)
