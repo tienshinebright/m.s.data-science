f93463b5f47797fb6240e01b5b322b43769090d4
zhangtienyong <tienhanprab@gmail.com>
2023-07-13 20:22:06 +0800

    list log

---
+++ python-crash-course/chapter_05/hello_admin.py
@@ -0,0 +1,6 @@
+username = ['admin', 'alice', 'bob', 'carera', 'danny']
+for name in username:
+    if name == 'admin':
+        print('Hellon ' + name + ' would you like to see a status report?')
+    else:
+        print('Hellon ' + name + ' thank you for logging in again.')
